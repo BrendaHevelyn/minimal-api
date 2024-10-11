566047d328c69a441710ec9922320e6b2e223ac9
BrendaHevelyn <brendahevelyn00@gmail.com>
2024-10-11 11:51:28 -0300

    :wrench: chore: Conexão com o banco de dados

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("ConexaoSqlServer")
+   
+);
+
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) => {
 
 if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     return Results.Ok("Login realizado com sucesso!");
