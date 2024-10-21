46fbcbee8706aa2313bc74a2ccfcca7a447d92b2
BrendaHevelyn <brendahevelyn00@gmail.com>
2024-10-21 11:05:47 -0300

    :sparkles: feat:Criados PUT e DELETE para veículo

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa244a30de47876ce6efe1a9f9b14590f032a084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b77f0f067135d2e354609bedb2d5ffffa19089f")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
